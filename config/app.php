0f4b54c130711ba631db904c3bcc92700db6d95a
shemaeric <shemaeric01@gmail.com>
2019-07-05 11:09:25 +0200

    host db

--- config/app.php
+++ config/app.php
@@ -43,7 +43,7 @@ return [
 
 
     // setup of error handling on heroku
-    'log'=>'errorlog'
+    'log'=>'errorlog',
 
     /*
     |--------------------------------------------------------------------------
--- config/database.php
+++ config/database.php
@@ -45,6 +45,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'safecity'),
